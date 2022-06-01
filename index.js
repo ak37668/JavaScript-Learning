3f0a2311becfdc2f14ee921bee197bad17771910
Ashok Meena <ak37668@gmail.com>
2022-06-01 15:58:17 +0530

    commit

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-// 1. ways to print in javascript
-console.log("Hello Ashok");
-alert('me');
-document.write("this is document write");
-
-// 2. JavaScript console API 
-console.log('hello world' , 50+15 , "Another Log");
-console.warn("this is warning");
-console.error("This is error");
-
-// 3. Javadcript variables
-// what are variable? - Containers to store data values
-var number1=54;
-var number2 = 35;
-var number3 = (number1 + number2);
-console.log(number3);
-
+    // 1. ways to print in javascript
+    console.log("Hello Ashok");
+    alert('me');
+    document.write("this is document write");
+    
+   // 2. JavaScript console API 
+    console.log('hello world' , 50+15 , "Another Log");
+    console.warn("this is warning");
+    console.error("This is error");
+    
+     // 3. Javadcript variables
+   // what are variable? - Containers to store data values
+    var number1=54;
+    var number2 = 35;
+    var number3 = (number1 + number2);
+    console.log(number3);
+    
+    //4. Data types in Javascript
+    //numbers
+    var num1 = 220;
+    var num2 = 78.5;
+    var num3 = (num1 + num2);
+    console.log(num3);
+    
+    //strings
+    let carName1 = "hundai i20";  // Double quotes
+    let carName2 = 'balano'; 
+    console.log(carName1 , carName2);
+  
