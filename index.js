d7039f88e42a1b560b9dd495bbe29ad2c0fbe8d4
Ashok Meena <ak37668@gmail.com>
2022-06-01 10:28:05 +0530

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// 1. ways to print in javascript
+console.log("Hello Ashok");
+alert('me');
+document.write("this is document write");
+
+// 2. JavaScript console API 
+console.log('hello world' , 50+15 , "Another Log");
+console.warn("this is warning");
+console.error("This is error");
+
+// 3. Javadcript variables
+// what are variable? - Containers to store data values
+var number1=54;
+var number2 = 35;
+var number3 = (number1 + number2);
+console.log(number3);
+
