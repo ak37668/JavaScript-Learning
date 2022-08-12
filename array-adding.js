0338d6dc2ffb82c78a817803258638fa3d0f7c07
Ashok Meena <106013780+ak37668@users.noreply.github.com>
2022-08-12 21:33:03 +0530

    Update array-adding.js

--- array-adding.js
+++ array-adding.js
@@ -3,6 +3,17 @@
 const cart = ['apple', 'orange'];
 cart.push('pear'); 
 
+
+
+
+// another examples of adding elements
+
 // Adding multiple elements:
 const numbers = [1, 2];
 numbers.push(3, 4, 5);
+
+
+const ingredients = ['eggs', 'flour', 'chocolate'];
+
+const poppedIngredient = ingredients.pop(); // 'chocolate'
+console.log(ingredients); // ['eggs', 'flour']
