b403c01980bd376bb29922e50486506a3dedc548
Ashok Meena <106013780+ak37668@users.noreply.github.com>
2022-10-29 15:17:38 +0530

    Create switch statement.js

---
+++ switch statement.js
@@ -0,0 +1,26 @@
+//=== comparision
+
+switch(input){
+    case 1: //if(input=== 1)
+    document.write("continue...")
+    break;
+
+    case "y": //if input === "y"
+    document.write("Continue...");
+    break;
+
+    case "yes": // if inout is === "yes"
+    document.write("continue...");
+    break;
+
+    case "n": // if input is ==="n"
+    document.write("end...");
+    break;
+
+    case "no": // is inout is ==="no"
+    document.write("End...");
+    break;
+
+    case 0: // wrong input
+    document.write("Wrong Input")
+}
