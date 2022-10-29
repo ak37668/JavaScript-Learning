bb3caba622779d2725845018bd52b8ebc39d96b8
Ashok Meena <106013780+ak37668@users.noreply.github.com>
2022-10-29 13:32:49 +0530

    push to github desktop

---
+++ elseif.js
@@ -0,0 +1,28 @@
+//if input = 1 , 'y', "yes" output = continue...
+//if inout = 0, "n" , "no" output = end...
+alert("Server Is Live")
+
+let input;
+
+input = "yes";
+
+if(input === 1)
+{document.write("continue...");}
+
+else if(input === "y")
+{document.write("continue...");}
+
+else if(input === "yes")
+{document.write("continue...");}
+
+else if(input === 0)
+{document.write("End...");}
+
+else if(input==="n")
+{document.write("End...") }
+
+else if(input==="No")
+{document.write("End...");}
+
+else
+{document.write("wrong input")};
