cfd40f695e11862d722248aab94bbb19f44f9f49
Ashok Meena <106013780+ak37668@users.noreply.github.com>
2022-11-01 16:12:29 +0530

    push to git

---
+++ boolean.js
@@ -0,0 +1,13 @@
+alert("this page is live")
+
+let acceptedTermsAndConditions = true;
+console.log(acceptedTermsAndConditions);
+
+let isGreater = 12 > 25;
+console.log(isGreater);
+
+// Yoda Notation
+number = 3;
+if (5 == number) {
+  console.log("equal");
+}
