722ea2eff6f687333a132e7c74d1be1f4b6f4014
Ashok Meena <106013780+ak37668@users.noreply.github.com>
2022-11-01 16:07:11 +0530

    update string.js

--- string.js
+++ string.js
@@ -2,7 +2,7 @@ alert("Server is live")
 
 let firstname = "Ashok";
 let lastname = "Meena";
-console.log(firstname + lastname);
+console.log(firstname + " " + lastname);
 
 console.log(`My name is ${firstname} ${lastname}`);
 console.log(`1 + 2 = ${1 + 2}`);
