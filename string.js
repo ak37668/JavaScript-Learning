8160fabe4ff30049af8ae0bf721aee34198410a7
Ashok Meena <106013780+ak37668@users.noreply.github.com>
2022-11-01 16:06:13 +0530

    pus to git

---
+++ string.js
@@ -0,0 +1,8 @@
+alert("Server is live")
+
+let firstname = "Ashok";
+let lastname = "Meena";
+console.log(firstname + lastname);
+
+console.log(`My name is ${firstname} ${lastname}`);
+console.log(`1 + 2 = ${1 + 2}`);
