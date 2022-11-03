ae5a804f4d94c776a2c8e9b4f4365655664591d9
Ashok Meena <106013780+ak37668@users.noreply.github.com>
2022-11-03 18:24:35 +0530

    push to git

---
+++ function name .js
@@ -0,0 +1,10 @@
+alert("function is live")
+
+function addToDate(date, month) {
+    // ...
+  }
+  
+  const date = new Date();
+  
+  // It's hard to tell from the function name what is added
+  addToDate(date, 1);
