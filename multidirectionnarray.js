7fc39cc2977fa6effbb69bbeb84c6e9f01ee5a6c
Ashok Meena <106013780+ak37668@users.noreply.github.com>
2022-12-01 07:27:15 +0530

    push to git

---
+++ multidirectionnarray.js
@@ -0,0 +1,14 @@
+alert('page is live to server');
+
+let book = ['MAths','science','physics','chemistry','computer science'];
+
+let bookswithpages =[
+    ["maths", "500"],
+    ["science", "600"],
+    ["physcis", "350"],
+    ["chemistry", "480"],
+    ["computer science", "450"]
+]
+
+let fetch = bookswithpages[4][0];
+console.log(fetch);
